98d5a459ded34620d539a5021fdf8733e805e167
Kristian Earl Contreras <kristianc@plumbersstock.com>
2022-08-28 19:45:53 +0800

    Max counters

---
+++ Counting Elements/MaxCounters.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/* 
+Performed by: https://github.com/kriscondev/
+
+Detected time complexity: O(N*M)
+Correctness 100%
+Performance 40%
+Task score 66%
+TOTAL SCORE 66%
+*/
+function solution($n, $array){
+    for ($i=0; $i < $n; $i++) { 
+        $maxCounter[] = 0; //setting initial value of zeroes 
+    }
+    
+    foreach($array as $k => $val){
+        // var_dump("val $val <= n $n");
+        if ($val <= $n){
+            $maxCounter[$val - 1] = $maxCounter[$val -1] + 1;
+        } else if($val == $n +1){
+            $max = max($maxCounter);
+            $keys = array_keys($maxCounter);
+            $maxCounter = array_fill_keys($keys,$max);
+
+        }    
+    }
+  
+    return $maxCounter;
+}
+
+// $n = 5;
+// $array = [3,4,4,6,1,4,4];
+// $array = [3,4];
+
+// $maxCounter = solution($n, $array);
